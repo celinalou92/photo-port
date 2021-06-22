d8e93ef32eb03af4095a51298e2b4aeb2da470c1
Celina Louissaint <celinalouissaint@gmail.com>
2021-06-21 18:33:58 -0700

    About component tests written

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+// functions from react testing library
+import { render, cleanup } from '@testing-library/react';
+// import extend-expect library from jest-dom
+import '@testing-library/jest-dom/extend-expect';
+// import the component we are testing
+import About from '..';
+
+// prevents leftover memory
+afterEach(cleanup);
+
+describe('About component', () => {
+    // verify the component is rerndering
+    it('renders', () => {
+        render(<About />);
+    });
+    // Second Test
+    it('matches snapshot DOM node structure', () => {
+        // render About snapshot
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
